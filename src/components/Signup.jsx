7dbc5b0d9d4dc69969a5be93d51ab24b624ad359
Nayem khan <nayem178548@gmail.com>
2025-12-04 23:34:10 +0000

    Created Input to reuse fields like email or password

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,7 @@
-export default function Signup() {
+import { useState } from "react";
 
+export default function Signup() {
+const [passwordAreNotEqual,setPasswordAreNotEqual]=useState(false);
     
 function handleSubmit(event){
 
@@ -8,6 +10,11 @@ function handleSubmit(event){
  const acquisitionChannel=formData.getAll('acquisition');
  const data= Object.fromEntries(formData.entries());
  data.acquisition=acquisitionChannel;
+if(data.password!==data['confirm-password']){
+setPasswordAreNotEqual(true);
+return;
+}
+
  console.log(data);
 event.target.reset();
 }
@@ -20,13 +27,13 @@ event.target.reset();
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email"   required />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" required minLength={8} />
         </div>
 
         <div className="control">
@@ -36,6 +43,7 @@ event.target.reset();
             type="password"
             name="confirm-password"
           />
+          <div className="control-error">{passwordAreNotEqual && <p>Passwords must match</p>}</div>
         </div>
       </div>
 
--- src/components/stateLogin.jsx
+++ src/components/stateLogin.jsx
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import Input from './Input';
 
 
 export default function Login() {
@@ -15,7 +16,7 @@ const [didEdit,setDidEdit]=useState({
   password:false
 })
 const emailIsInvalid=didEdit.email && !enteredValues.email.includes('@')
-
+const passwordIsInvalid=didEdit.password && enteredValues.password.trim().length<6;
 function handleSubmission(e){
   e.preventDefault();
  
@@ -57,29 +58,27 @@ setDidEdit((prevEdit)=>({
       <h2>Login</h2>
 
       <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input 
-          id="email" 
-          type="email" 
-          name="email" 
-          onBlur={()=>handleInputBlur('email')}
-           onChange={(event)=>handleInputChange('email',event.target.value)} 
-           value={enteredValues.email} 
+<Input 
+ label="email"
+ id="email"
+ type="email" 
+ name="email"
+ onBlur={()=>handleInputBlur('email')}
+ onChange={(event)=>handleInputChange('email',event.target.value)} 
+ value={enteredValues.email}
+   error={emailIsInvalid && 'please enter a valid email'}        
            />
-<div className='control-error'>{emailIsInvalid && <p>please enter a valid email address</p>}</div>
-        </div>
-
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input 
-          id="password" 
-          type="password" 
-          name="password" 
-         onChange={(event)=>handleInputChange('password',event.target.value)} 
+<Input 
+ label="password"
+ id="password"
+ type="password"
+ name="password"
+ onBlur={()=>handleInputBlur('password')}
+ onChange={(event)=>handleInputChange('password',event.target.value)} 
            value={enteredValues.password} 
-          />
-        </div>
+    error={passwordIsInvalid && 'please enter a valid password'}       
+           />
+
       </div>
 
       <p className="form-actions">
